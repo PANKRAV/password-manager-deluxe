19a80ecd6c196a2dbd29fea84f044c77c1aab4ef
PANKRAV <pankravaris@gmail.com>
2022-11-06 13:54:59 +0200

    Restructured the project and started documenting the code

---
+++ scripts/modules/_utility.py
@@ -0,0 +1,362 @@
+#PYTHONMODULES
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from ast import Tuple
+import json
+from pathlib import Path
+import asyncio
+import os
+import sys
+import time
+from typing import List, Dict
+import threading
+import functools
+import concurrent.futures
+import logging
+from msvcrt import getch, getche
+import atexit
+
+
+
+
+#MYMODULES
+from .variables import ReadOnly, user_data_init, global_security_init, global_logging_init, users_to_reset_init
+
+
+
+
+
+
+
+
+
+
+
+
+def _init() -> Dict:
+    global format
+
+    
+
+    abspath = Path(os.path.abspath(__file__))
+    os.chdir(abspath.parent.parent.parent)
+
+    global_security_init()
+    user_data_init()
+    global_logging_init()
+    users_to_reset_init()
+
+    from .variables import GLOBAL_LOGGING
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=GLOBAL_LOGGING,
+                        datefmt="%H:%M:%S")
+
+
+    dirs = os.listdir()
+    if "setup" not in dirs :
+        with open("setup", "x"):
+            pass
+
+        try :
+            with Path(".env").open(mode = "x"):
+                pass
+        except :
+            print("password manager is not properly setted up")
+
+
+        if "data" not in dirs:
+            os.mkdir("data")
+
+
+        with Dir_Reset.from_string("data") as cur :
+            if "user_data" not in cur.dirs :
+                os.mkdir("user_data")
+            
+            if "password_data" not in cur.dirs :
+                os.mkdir("password_data")
+
+            if "encryption_data" not in cur.dirs :
+                os.mkdir("encryption_data")
+
+            with Dir_Reset.from_string("user_data") as _cur :
+                if "users.json" not in _cur.dirs :
+                    with open("users.json", "x") :
+                        pass
+        
+
+def _quit() -> None :
+    sys.exit(0)
+
+
+
+#To prevent hash timing attacks
+def scheduled_return(_func = None,* ,interval : float = .2, _timeout : float|int = 100) :
+    def inner(func) :
+
+       
+        mybarrier = threading.Barrier(2)
+        myevent = threading.Event()
+
+        def delay(interval) :
+            while not myevent.is_set() :
+                logging.debug("started sleeping")
+                time.sleep(interval)
+                logging.debug("checking for event")
+                
+            try :
+                mybarrier.wait(timeout=100)
+                
+
+            except concurrent.futures.TimeoutError :
+                logging.warning("delay timed out")
+                return False
+
+            except Exception as ex:
+                raise ex(f"An exception occurred: {ex}")
+
+            return
+
+
+        class FuncData :
+            def __init__(self, datafunc, _args, _kwargs) :
+                self.datafunc : function = datafunc
+                self.args : List = _args
+                self.kwargs : Dict = _kwargs
+        
+        def new_func(data : FuncData) :
+            global val
+            val = None
+            func = data.datafunc
+            args = data.args
+            kwargs = data.kwargs
+            start_time = time.time()
+            logging.info(f"starting process : {func.__name__}(args={args}, kwargs={kwargs})")
+            try :
+                val = func(*args, **kwargs)
+            except Exception as ex :
+                logging.warning(f"Ignoring Exception: {ex} occured in the decorated function : {func.__name__}(args={args}, kwargs={kwargs}), (returning None)")   
+                myevent.set()
+                return     
+            myevent.set()
+            mybarrier.wait()
+            logging.info(f"Finished process : {func.__name__}(args={args}, kwargs={kwargs}) in {time.time() - start_time}")
+            return val
+            
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs) :
+            global mybarrier
+            data = FuncData(func, args, kwargs)
+            myevent.clear()
+            mybarrier = threading.Barrier(2)
+            with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+                executor.map(new_func, (data, ))
+                try :
+                    executor.map(delay, (interval, ), timeout=_timeout)
+                except concurrent.futures.TimeoutError :
+                    mybarrier.wait()
+                    logging.warning("Delay timed out")
+            
+            logging.debug("exited successfully")
+            return val
+
+        
+        return wrapper
+
+    if _func is None :  
+        return inner
+    else :
+        return inner(_func)
+
+
+
+
+def handle_file(path : Path, opt : str = "fetch", mode : str = "norm") -> dict :
+    if mode == "bytes" :
+        mode = "b"
+    else :
+        mode = "t"
+
+
+    if opt == "make" :
+        with Dir_Reset(path) as cur :
+            if path not in cur.dirs :
+                with path.open(f"x{mode}") as f_w:
+                    _json = {}
+                    f_w.write(json.dumps(_json))
+    
+    if opt == "fetch" :
+        with Dir_Reset(path) as cur :
+            if path in cur.dirs :
+                with path.open(f"r{mode}") as f_r:
+                    _json = f_r.read()
+                    _json = json.loads(_json)
+
+
+    return _json
+
+def finalize() :
+    from .user import User
+    from .admin import Admin
+    print("finalizing")
+    User.reset_encryption()
+    Admin.reset_admin()
+
+async def minimal_time(func, delay) :
+    ...
+
+
+class function :
+        ...
+class Arg_Save_Wrapper :
+    
+
+    def __init__(self, func : function) :
+        self.func = func
+
+    def logargs(self, *args, **kwargs) :
+
+        self.func.args = f"*args = ({args}), *kwargs = ({kwargs})"
+        self.func.__call__(*args, *kwargs)
+
+
+
+def argssave(func : function) :
+    return Arg_Save_Wrapper(func).logargs
+
+
+
+def timeit(func : function, debug : bool = True, verbose : bool = False, *args : List[function], **kwargs : Dict[str, function]
+            ) -> function | List[function] :
+
+    error_count = 0
+    errors = dict()
+    start_time = time.time()
+    try :
+        _val = func()
+
+    except Exception as ex :        
+        error_count += 1
+        print(f"{ex} occurred in 1st process")
+        errors[f"{error_count}.{_val.__name__}()"] = ex
+
+    if args != None or kwargs != None :
+        _val = list(_val)
+
+        for idx, _func in enumerate(args, start = 2) :
+            _func : function
+            try :
+                _val.append(_func())
+            except Exception as ex:
+                print(f"{ex} occured in No.{idx} process ({_func.__name__}())")
+                errors[f"{error_count}.{_func.__name__}()"] = ex
+
+
+        for idx, item in enumerate(kwargs.items(), start = 2 + len(args)) :
+            key, _func = item
+            try :
+                val.append(_func().item())
+                print(f"No.{idx} ({_func.__name__}()) {key} process executed successfully")
+            except Exception as ex:
+                print(f"{ex} occured in No.{idx} {key} process ({_func.__name__}())")
+                errors[f"{error_count}.{_func.__name__}()"] = ex
+
+
+    _time = time.time() - start_time
+    if debug :
+        print(f"{1 + len(args) + len(kwargs)} Prosseses were executed in {_time} with {error_count} error(s)")
+
+    if verbose :
+        for key, ex in errors :
+            print(f"{ex} occured in {key} process")
+
+    return val
+
+
+def loop_switch() -> None :
+    os.system("cls||clear")
+
+
+class Timeout :
+
+    def __init__(self, _interval : float|int) -> None:
+        self._interval = _interval
+        self._event = threading.Event()
+
+
+    def sleep(self) :
+        time.sleep(self._interval)
+        self._event.set()
+        return
+
+    def __enter__(self) :
+        with concurrent.futures.ThreadPoolExecutor(max_workers=1) as exec :
+            exec.map(self.sleep, ())
+
+
+        
+    def __exit__(self, exc_type, exc_val, exc_tb) :
+        loop_switch()
+
+
+class Dir_Reset :
+    root = Path(__file__).parent.parent.parent
+
+
+    def __init__(self, path : Path) -> None:
+        self.path = path
+
+
+
+    @property
+    def dirs(self) :
+        return os.listdir(os.getcwd())
+
+    @dirs.setter
+    def dirs(self, value) :
+        raise ReadOnly("dirs attribute cannot be setted")
+
+
+    @property
+    def pathlibdirs(self) :
+        return get_dirs(os.getcwd())
+
+    @pathlibdirs.setter
+    def pathlibdirs(self, value) :
+        raise ReadOnly("pathlibdirs attribute cannot be setted")
+
+
+    def __enter__(self) :
+        if not self.path == self.root :       
+            os.chdir(self.path)
+            return self
+
+
+
+    def __exit__(self, exc_type, exc_val, exc_tb) :
+        if not self.path == self.root :
+            os.chdir(Dir_Reset.root)
+
+
+    @classmethod
+    def from_string(cls, _path : str) :
+        _path = Path(_path)
+        return cls(_path)
+
+
+def get_dirs(path = os.getcwd()) -> Tuple :
+    dirs = os.listdir(path)
+    dirs = tuple(Path(dir) for dir in dirs)
+
+    return dirs
+
+
+
+
+
+class AsterisksOut :
+
+    def __enter__(self) :
+        ...
+
+    def __exit__(self, exc_type, exc_val, exc_tb) :
+        ...
---
+++ scripts/modules/encryption.py
@@ -0,0 +1,321 @@
+#PYTHONMODULES
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from numpy import character, random as nprand
+import numpy as np
+import hashlib
+from rsa import PrivateKey, PublicKey
+import cryptography
+import simplecrypt
+import rsa
+from cryptography.fernet import Fernet
+import pathlib
+import json
+
+
+#MYMODULES
+from .variables import CHARS, LOWER, UPPER, NUMBERS, SYMBOLS, RAN_CHAR_SEQ, BadUserSetup, GLOBAL_SECURITY
+from ._utility import scheduled_return
+
+
+@scheduled_return
+def random_password(length = 10):
+    pr = []
+
+    char_list = [char for char in CHARS]
+    LOWER_list = [char for char in LOWER]
+    UPPER_list = [char for char in UPPER]
+    NUMBERS_list = [char for char in NUMBERS]
+    SYMBOLS_list = [char for char in SYMBOLS]
+
+    const = 1/len(char_list)
+
+    for char in char_list:
+        
+        if char in LOWER_list:
+            pr.append(0.2/len(LOWER_list))
+
+
+        elif char in UPPER_list:
+            pr.append(0.2/len(UPPER_list))
+
+
+        elif char in NUMBERS_list:
+            pr.append(0.3/len(NUMBERS_list)) 
+
+        
+        elif char in SYMBOLS_list:
+            pr.append(0.3/len(SYMBOLS_list)) 
+
+        
+    password = nprand.choice(char_list, p = pr, size = length) 
+
+    return "".join(password)
+
+
+
+
+@scheduled_return
+def random_password_new(length = 10):
+    pr = []
+    last = None
+    password = []
+
+    char_list = [char for char in CHARS]
+    LOWER_list = [char for char in LOWER]
+    UPPER_list = [char for char in UPPER]
+    NUMBERS_list = [char for char in NUMBERS]
+    SYMBOLS_list = [char for char in SYMBOLS]
+
+    const = 1/len(char_list)
+
+    for char in char_list:
+        
+        if char in LOWER_list:
+            pr.append(0.2/len(LOWER_list))
+
+
+        elif char in UPPER_list:
+            pr.append(0.2/len(UPPER_list))
+
+
+        elif char in NUMBERS_list:
+            pr.append(0.3/len(NUMBERS_list)) 
+
+        
+        elif char in SYMBOLS_list:
+            pr.append(0.3/len(SYMBOLS_list)) 
+
+    for i in range(length):        
+        char = nprand.choice(char_list, p = pr, size = 1) 
+
+        if char in LOWER_list:
+            if last == 1 and repeat == True:
+                char = nprand.choice(char_list, p = pr, size = 1)
+                repeat = False
+                
+            else:
+                last = 1
+
+        
+        elif char in UPPER_list:
+            if last == 2 and repeat == True:
+                char = nprand.choice(char_list, p = pr, size = 1)
+                repeat = False
+                
+            else:
+                last = 2
+
+        
+        elif char in NUMBERS_list:
+            if last == 3 and repeat == True:
+                char = nprand.choice(char_list, p = pr, size = 1)
+                repeat = False
+                
+            else:
+                last = 3
+
+        
+        elif char in SYMBOLS_list:
+            if last == 4 and repeat == True:
+                char = nprand.choice(char_list, p = pr, size = 1)
+                repeat = False
+                
+            else:
+                last = 4
+        
+        password.append(char)
+        repeat = True
+
+    password = np.concatenate(password).ravel().tolist()
+    return "".join(password)
+
+#hash processes
+@scheduled_return(interval=.4)
+def hash2(ctx) -> str:
+    byte_data = ctx.encode()
+    return hashlib.sha512(byte_data).hexdigest()
+
+@scheduled_return(interval=.4)
+def hash3(ctx) -> str:
+    byte_data = ctx.encode()
+    return hashlib.sha3_512(byte_data).hexdigest()
+
+def salt(ctx : str) -> str:
+    salt = random_password(5)
+    return ctx + salt, salt
+
+
+@scheduled_return(interval=1)
+def simple_crypt(passkey, ctx, mode : str = "enc") -> str:
+    
+    if mode == "enc":
+        return simplecrypt.encrypt(passkey, ctx)
+
+    elif mode == "dec":
+        return simplecrypt.decrypt(passkey, ctx)
+
+    else:
+        return None
+
+
+
+
+@scheduled_return(interval=.01)
+def ceasar(ctx, indent : int = 0) -> str:
+    ctx = list(ctx)
+
+    for idx, char in enumerate(ctx):
+
+        char:str
+        if not char.isspace():
+            #ran_char_seq =  was assigned at line 22
+            _index = RAN_CHAR_SEQ.index(char)
+            try:
+                new_char = RAN_CHAR_SEQ[_index + indent]
+            
+            except IndexError:
+                if _index + indent > 72:
+                    new_char = RAN_CHAR_SEQ[indent]
+                
+                elif _index + indent < 0:
+                    new_char = RAN_CHAR_SEQ[72 - indent]
+            ctx[idx] = new_char
+
+    return "".join(ctx)
+
+
+@scheduled_return(interval=.01)
+def reverse_ceasar(ctx, indent : int = 0) -> str:
+
+    return ceasar(ctx , -indent)
+
+
+class Encryption :
+    
+
+    @staticmethod
+    class SinglePwd :
+        if TYPE_CHECKING :
+            from user import User
+    
+
+        def __init__(self, user : User, ctx : str) -> None:
+            self.user = user
+            self.key = self.user.key
+            if not self.user.key_check :
+                raise BadUserSetup("User object doesn't have an encryption key")
+
+            else :
+                self.enc_content = ctx.encode("latin-1")
+
+                self.enc_json = json.loads(self.user.enc_json)
+                self.publicKey = self.enc_json["publicKey"]
+                self.publicKey = self.publicKey.encode("latin-1")
+                self.publicKey = simple_crypt(self.key, self.publicKey, "dec")
+                self.publicKey = PublicKey.load_pkcs1(self.publicKey)
+                self.publicKey : PublicKey
+
+
+                self.privateKey = self.enc_json["privateKey"]
+                self.privateKey = self.privateKey.encode("latin-1")
+                self.privateKey = simple_crypt(self.key, self.privateKey, "dec")
+                self.privateKey = PrivateKey.load_pkcs1(self.privateKey)
+                self.privateKey : PrivateKey
+
+                self.dec_content = rsa.decrypt(self.enc_content, self.privateKey).decode()
+
+        
+
+
+
+
+        @classmethod
+        def new_pwd(cls, user : User, ctx : str) :
+            ...
+
+
+
+    
+
+    class UserEnc :
+        if TYPE_CHECKING :
+            from .user import User
+
+        
+        def __init__(self, user : User) -> None:
+            publicKey, privateKey = rsa.newkeys(GLOBAL_SECURITY)
+            
+            self.user = user
+            self.key = self.user.key
+            self.enc_json = json.loads(self.user.enc_json)
+
+
+            self.publicKey = self.enc_json["publicKey"]
+            self.publicKey = self.publicKey.encode("latin-1")
+            self.publicKey = simple_crypt(self.key, self.publicKey, "dec")
+            self.publicKey = PublicKey.load_pkcs1(self.publicKey)
+
+            self.privateKey = self.enc_json["privateKey"]
+            self.privateKey = self.privateKey.encode("latin-1")
+            self.privateKey = simple_crypt(self.key, self.privateKey, "dec")
+            self.privateKey = PrivateKey.load_pkcs1(self.privateKey)
+            self.privateKey : PrivateKey
+
+
+
+
+
+            del publicKey, privateKey
+
+
+
+
+        def print_data(self) -> str:
+            
+
+
+            _json = json.loads(self.user.pwd_json)
+
+
+            dec_json = _json.copy()
+
+            for key, item in dec_json.items() :
+                
+                new_pwd = rsa.decrypt(dec_json[key]["pwd"].encode("latin-1"), self.privateKey)
+
+                dec_json[key]["pwd"] = new_pwd.decode("latin-1")
+
+            return json.dumps(dec_json)
+
+
+
+        def reset(self) :
+            ...
+
+
+        
+        @staticmethod
+        def user_init(key : str) :
+            publicKey, privateKey = rsa.newkeys(GLOBAL_SECURITY)
+
+
+            publicKey = simple_crypt(key, publicKey.save_pkcs1())
+            publicKey = publicKey.decode("latin-1")
+
+
+            privateKey = simple_crypt(key, privateKey.save_pkcs1())
+            privateKey = privateKey.decode("latin-1")
+
+            return publicKey, privateKey, GLOBAL_SECURITY
+
+    
+        def encrypt(self, pwd : str) :
+            return rsa.encrypt(pwd.encode("latin-1"),
+                    self.publicKey).decode("latin-1")
+
+
+        def decrypt(self, pwd : str) :
+            return rsa.decrypt(pwd.encode("latin-1"), self.privateKey).decode("latin-1")
+class Password :
+    ...
